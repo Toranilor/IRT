7400bf27c322c0f62240c2a1960cfda7edfe8c5b
Tom Dixon <toranilor@gmail.com>
2019-02-25 11:46:49 +1100

    test_calibrate written
    test_calibrate performs well on phil dataset and on textbook dataset.

---
+++ IRT_Utils.py
@@ -0,0 +1,120 @@
+"""
+A series of item response theory utilities.
+Written using equations from "The Basics of Item Response Theory Using R"
+by F. Baker and S. Kim.
+
+Tom Dixon - thomas.dixon@unsw.edu.au
+Created: 8/01/2019
+Last Modified: 10/01/2019
+
+Some common variable definitions:
+theta - ability of student(s)
+r - response(s) of students (a mark) to a particular item
+f - total attempt(s) for a particular item
+a - discrimination of a particular item ("slope" of IRT curve roughly speaking)
+b - difficulty of a particular item
+c - chance of guessing the correct answer for a particular item
+"""
+
+import scipy as sp 
+import numpy as np 
+
+def estimate_ability_l1(b, u, n_iter=100, theta=0):
+    """
+    A function to estimate the ability of a student, assuming the test items
+    follow a 1st degree logistic.
+
+    b is a j-length vector of the difficulty for each item
+    u is a j-length vector of the student's response for each item (between 0 and 1)
+    n_iter is the number of iterations of ability to perform
+
+    Using equations from page 70 of "The Basics of Item Response Theory Using R"
+    """
+    assert(b.shape == u.shape)
+
+    for i in range(n_iter):
+        Prob = logistic_1(theta, b)
+        Q = 1-Prob
+        numerator = np.sum(u-Prob)
+        denominator = -np.sum(Prob*Q)
+        theta -= numerator/denominator
+
+    return theta
+
+def estimate_ability_l2(b, a, u, n_iter=100, theta=0):
+    # I NEED TO WRITE THIS
+    return theta
+
+
+def l1_wrapper(x, **kwargs):
+    """
+    A wrapper function for logistic_1, to allow it to be used in scipy.optimize.
+    'x' is our estimate for b
+    'kwargs' contains:
+        theta vector
+        r/f vector
+    """
+    assert np.size(x) == 1
+    b = x
+    theta = kwargs.get('theta')
+    r_on_f = kwargs.get('r_on_f')
+
+    # We return the residual back to scipy for use in optimisation
+    # (Need to reshape it from a (X,1) sized array into a 1D array...)
+    residuals = np.reshape((logistic_1(theta, b) - r_on_f), np.size(theta))
+    return residuals
+
+def l2_wrapper(x, **kwargs):
+    """
+    A wrapper function for logistic_2, to allow it to be used in scipy.optimize.
+    'x' is our estimate for a and b
+    'kwargs' contains:
+        theta vector
+        r/f vector
+    """
+    assert np.size(x) == 2
+    a = x[0]
+    b = x[1]
+
+    theta = kwargs.get('theta')
+    r_on_f = kwargs.get('r_on_f')
+
+    # We return the residual back to scipy for use in optimisation
+    # (Need to reshape it from a (X,1) sized array into a 1D array...)
+    residuals = np.reshape((logistic_2(theta, a, b) - r_on_f), np.size(theta))
+    return residuals
+
+
+def logistic_1(theta, b):
+    """
+    Function for an item response curve of 1 variable.
+    Also known as a Rasch model. 
+    This function returns the probability that a student of ability theta
+    would correctly get the answer to a question of difficulty b. 
+    It assumes that the discrimination (a) is 1.
+    """
+    logistic = np.asarray(theta)-np.asarray(b)
+    prob = 1/(1+np.exp(-logistic))
+    return prob
+
+def logistic_2(theta, a, b):
+    """
+    Function for an item response curve of two variables.
+    Returns the probability that a student of ability theta would correctly
+    get the answer to a quesiton of difficulty b and discrimination a.
+    """
+    logistic = np.asarray(theta)-b
+    prob = 1/(1+np.exp(-a*logistic))
+    return prob
+
+
+def logistic_3(theta, a, b, c):
+    """
+    Function for an item response curve of three variables.
+    Returns the probability that a student of ability theta would correctly
+    get the answer to a question of difficulty b, discrimination a, and a
+    probability to guess the right answer of c.
+    """
+    logistic = np.asarray(theta)-b
+    prob = c + (1-c)*1/(1+np.exp(-a*logistic))
+    return prob
--- test_calibrate.py
+++ test_calibrate.py
@@ -15,17 +15,124 @@ a - discrimination of a particular item ("slope" of IRT curve roughly speaking)
 b - difficulty of a particular item
 c - chance of guessing the correct answer for a particular item
 
+
+TODO - fix the necessicty of reshaping everything before passing into functions!
 """
-import IRT_Utils as IRT 
+from IRT import IRT_Utils as IRT
 import numpy as np
 from matplotlib import pyplot as plt
 import random
 import scipy.optimize
 
 
-def apply_calibration(dataset, n_param=1):
-	"""
-	This utility applies calibration to the test:
-		dataset is a numpy array with student data 
-	"""
+def apply_calibration(scores, n_param=1, n_iter=1000, qns_per_student=0):
+    """
+    This utility applies calibration to the test:
+    	dataset is a numpy array with student data, scores for each item should be out of 1
+    n_param is the number of parameters of logistic model we are using.
+    """
+    # Assign functions based on n_param
+    if n_param == 1:
+        # 1 dimensional model, aka rach model
+        curve_func = IRT.logistic_1
+        fit_func = IRT.l1_wrapper
+    if n_param == 2:
+        # 2 dimensional model, with both dificulty and discrimination.
+        curve_func = IRT.logistic_2
+        fit_func = IRT.l2_wrapper
+    if n_param == 3:
+        # 3 dimensional model, difficulty, discrimination and guess-correctly chance
+        curve_func = IRT.logistic_3
+        fit_func = IRT.l3_wrapper
 
+    num_students = scores.shape[0]
+    num_questions = scores.shape[1]
+
+    # Generate a score vector (total scores of all people)
+    user_scores = np.zeros((num_students, 1))
+    for i in range(num_students):
+        user_scores[i] = np.sum(np.nan_to_num(scores[i, :]), 0)
+
+    # Generate a difficulty vector (beginning estimate!)
+    q_diff = np.zeros((num_questions, 1))
+    guess_init = 0
+    if n_param > 1:
+        # Generate a discrimination vector
+        q_disc = np.zeros((num_questions, 1))
+        guess_init = [0, 1]
+    if n_param > 2:
+        # Generate a guessing vector
+        q_chance = np.zeros((num_questions, 1))
+        guess_init = [0, 1, 0.25]
+
+    sum_score = np.zeros((num_questions, 1))
+    for i in range(num_questions):
+        q_diff[i] = 3-np.sum(scores[:, i])/num_students*6
+        if n_param > 1:
+            q_disc[i] = 1
+        if n_param > 2:
+            q_chance[i] = 0.25
+        sum_score[i] = np.sum(np.nan_to_num(scores[:, i]), 0)
+
+    if qns_per_student == 0:
+        # You haven't passed a value for questions_per_student, so I 
+        # am assuming that it's the number of qeustions - every student takes every question
+        qns_per_student = num_questions
+
+    ### Begin the two-part iterative process!
+    # Generate an initial estimate for student ability values (theta)
+
+    theta_estimates = np.log(user_scores/(qns_per_student-user_scores))[:, 0]
+    print('Beginning Iterations')
+
+    for i in range(n_iter):
+        print("iteration" + str(i))
+        # Perform an estimate of question difficulty based on student responses
+        for j in range(num_questions):
+            # Compress responses (remove students who did not attempt this q)
+            inclusion_IDs = np.argwhere(~np.isnan(scores[:, j]))
+            # Extract the relevant thetas
+            theta_temp = theta_estimates[inclusion_IDs]
+            scores_temp = scores[inclusion_IDs, j]
+
+            # Group close-by thetas together (?)
+
+            # Generate kwargs to pass to logistic function
+            kwargs = {
+                "theta": theta_temp,
+                "r_on_f": scores_temp
+            }
+
+            q_guess = scipy.optimize.least_squares(fit_func, x0=guess_init, kwargs=kwargs).x
+
+            q_diff[j] = q_guess[0]
+            if n_param > 1:
+                q_disc[j] = q_guess[1]
+            if n_param > 2:
+                q_chance = q_guess[2]
+
+        # "Anchor" our metric
+        q_diff = q_diff - np.mean(q_diff)
+
+        # Perform an estimate of student ability based on question difficulty      
+        for j in range(num_students):
+            # Compress responses again (remove questions the student did not attempt)
+            inclusion_IDs = np.argwhere(~np.isnan(scores[j, :]))
+            if n_param == 1:
+                theta_estimates[j] = IRT.estimate_ability_l1(
+                    theta=theta_estimates[j], b=q_diff[inclusion_IDs].reshape((np.size(inclusion_IDs), 1)), 
+                    u=scores[j, inclusion_IDs].reshape((np.size(inclusion_IDs), 1)), n_iter=1)
+            if n_param == 2:
+                theta_estimates[j] = IRT.estimate_ability_l2(
+                    theta=theta_estimates[j], b=q_diff[inclusion_IDs].reshape((np.size(inclusion_IDs), 1)), 
+                    a=q_disc[inclusion_IDs],
+                    u=scores[j, inclusion_IDs].reshape((np.size(inclusion_IDs), 1)), n_iter=1)
+            if n_param == 2:
+                theta_estimates[j] = IRT.estimate_ability_l3(
+                    theta=theta_estimates[j], b=q_diff[inclusion_IDs], 
+                    a=q_disc[inclusion_IDs], c=q_chance[inclusion_IDs],
+                    u=scores[j, inclusion_IDs].reshape((np.size(inclusion_IDs), 1)), n_iter=1)
+
+    # Another correction from the book
+    theta_estimates = theta_estimates*(num_questions-2)/(num_questions-1)
+    return q_diff, theta_estimates
